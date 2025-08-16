3a16b4e7993da9faa8583e572dc2fb28c50d54c0
mehdi95zeid <mehdi95zeid@gmail.com>
2025-08-16 17:34:00 +0330

    fix: minor fixes and cleanup

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,14 +1,9 @@
 import type { Route } from "./+types/Home";
-import Product from "../components/Product";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Moon" }, { name: "description", content: "since 1650" }];
 }
 
 export default function Home() {
-  return (
-    <>
-      <Product />
-    </>
-  );
+  return <></>;
 }
